61b859f8c0a3a2ec1e0f48dbbd6981e1932fe484
Mandzheev <mandzheevdmitriy@gmail.com>
2021-06-09 13:11:31 +0500

    1

---
+++ kaleidoscope.cxx
@@ -0,0 +1,42 @@
+#include<string>
+
+// The lexer returns tokens [0-255] if it is an unknown character, otherwise one
+// of these for known things.
+enum Token {
+    tok_eof = -1,
+
+    // commands
+    tok_def = -2,
+    tok_extern = -3,
+    
+    // primary
+    tok_identifier = -4,
+    tok_number = -5,
+
+};
+
+static std::string IdentifierStr; // Filled in if tok_identifier
+static double NumVal;             // Filled in if tok_number
+
+// gettok - Return the next token from standard input
+static int gettok() {
+    static int LastChar = ' ';
+
+    // Skip any whitespace
+    while (isspace(LastChar))
+        LastChar = getchar();
+
+    if (isalpha(LastChar)) { // identifier: [a-z A-Z][a-z A-Z 0-9]*
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+
+        if (IdentifierStr == "def")
+            return tok_def;
+        if (IdentifierStr == "extern")
+            return tok_extern;
+        return tok_identifier;
+    }
+}
+
+
