cc91d07eb19f3e9db3f7cbc4283213a09a072f5b
Mandzheev <mandzheevdmitriy@gmail.com>
2021-06-10 18:09:23 +0500

    1

--- kaleidoscope.cxx
+++ kaleidoscope.cxx
@@ -1,17 +1,17 @@
-#include<string>
+#include <string>
 
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
 enum Token {
-    tok_eof = -1,
+  tok_eof = -1,
 
-    // commands
-    tok_def = -2,
-    tok_extern = -3,
-    
-    // primary
-    tok_identifier = -4,
-    tok_number = -5,
+  // commands
+  tok_def = -2,
+  tok_extern = -3,
+
+  // primary
+  tok_identifier = -4,
+  tok_number = -5,
 
 };
 
@@ -20,23 +20,34 @@ static double NumVal;             // Filled in if tok_number
 
 // gettok - Return the next token from standard input
 static int gettok() {
-    static int LastChar = ' ';
+  static int LastChar = ' ';
 
-    // Skip any whitespace
-    while (isspace(LastChar))
-        LastChar = getchar();
+  // Skip any whitespace
+  while (isspace(LastChar))
+    LastChar = getchar();
 
-    if (isalpha(LastChar)) { // identifier: [a-z A-Z][a-z A-Z 0-9]*
-        IdentifierStr = LastChar;
-        while (isalnum((LastChar = getchar())))
-            IdentifierStr += LastChar;
+  if (isalpha(LastChar)) { // identifier: [a-z A-Z][a-z A-Z 0-9]*
+    IdentifierStr = LastChar;
+    while (isalnum((LastChar = getchar())))
+      IdentifierStr += LastChar;
 
-        if (IdentifierStr == "def")
-            return tok_def;
-        if (IdentifierStr == "extern")
-            return tok_extern;
-        return tok_identifier;
-    }
+    if (IdentifierStr == "def")
+      return tok_def;
+    if (IdentifierStr == "extern")
+      return tok_extern;
+    return tok_identifier;
+  }
+
+  if (isdigit(LastChar) || LastChar == '.') { // Number: [0-9.]
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+
+    NumVal = strtod(NumStr.c_str(), 0); // convert string to a numeric value
+    return tok_number;
+  }
+
+
 }
-
-
